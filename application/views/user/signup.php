96237be47bff44a7951a246c4c30cedcd00c2fd2
macadamia94 <thswlgus15@hanmail.net>
2022-06-28 01:04:16 +0900

    .

--- application/views/user/signup.php
+++ application/views/user/signup.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once "application/views/template/head.php"; ?>
-<body>
+<body class="h-full container-center">
     <div>
-        <h1>로그인</h1>
+        <h1>회원가입</h1>
 
-        <form action="@{signin}" method="post">
+        <form action="signup" method="post">
             <div><input type="email" name="email" placeholder="email" autofocus required></div>
             <div><input type="password" name="pw" placeholder="password" required></div>
+            <div><input type="text" name="nm" placeholder="name" require></div>
             <div>
-                <input type="submit" value="로그인">
+                <input type="submit" value="회원가입">
             </div>
         </form>
         <div>
-            <a href="signup">회원가입</a>
+            <a href="signin">로그인</a>
         </div>
     </div>
 </body>
