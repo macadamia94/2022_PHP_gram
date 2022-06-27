195090e8591f8870fb85642af078ecaf95ad7a37
macadamia94 <thswlgus15@hanmail.net>
2022-06-27 16:54:12 +0900

    feed

--- application/controllers/FeedController.php
+++ application/controllers/FeedController.php
@@ -3,9 +3,8 @@ namespace application\controllers;
 
 class FeedController extends Controller {
     public function index() {
-
+        $this->addAttribute(_JS, ["feed/index"]);
         $this->addAttribute(_MAIN, $this->getView("feed/index.php"));
-        
         return "template/t1.php";
     }
 }
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,7 +10,7 @@
             <div class="d-inline-flex flex-grow-1 flex-shrink-0">
                 <nav class="d-flex flex-grow-1 flex-column flex-md-row justify-content-end">
                     <div class="d-inline-flex me-3"> <!-- me-3 : margin right -->
-                        <a href="#" id="btnNewFeedModel" data-bs-toggle="modal" data-bs-target="#newFeedModal">
+                        <a href="#" id="btnNewFeedModal" data-bs-toggle="modal" data-bs-target="#newFeedModal">
                             <!-- svg : 그림으로 된 폰트 -->
                             <svg aria-label="새로운 게시물" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line></svg>
                         </a>
@@ -43,3 +43,21 @@
         </div>
     </div>
 </header>
+
+<!-- New Feed Create Modal -->
+<div class="modal fade" id="newFeedModal" tabindex="-1" aria-labelledby="newFeedModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content" id="newFeedModalContent">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newFeedModalLabel">새 게시물 만들기</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="id-modal-body"></div>
+
+            <form class="d-none">
+                <input type="file" accept="image/*" name="imgs" multiple>
+            </form>
+        </div>
+    </div>
+</div>
+
