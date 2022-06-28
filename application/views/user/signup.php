f890656a8e1b6316d034f8028073e77c147d23ce
macadamia94 <thswlgus15@hanmail.net>
2022-06-28 23:43:59 +0900

    tab change

--- application/views/user/signup.php
+++ application/views/user/signup.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once "application/views/template/head.php"; ?>
-<body class="h-full container-center">
-    <div>
-        <h1>회원가입</h1>
 
-        <form action="signup" method="post">
-            <div><input type="email" name="email" placeholder="email" autofocus required></div>
-            <div><input type="password" name="pw" placeholder="password" required></div>
-            <div><input type="text" name="nm" placeholder="name" require></div>
-            <div>
-                <input type="submit" value="회원가입">
-            </div>
-        </form>
-        <div>
-            <a href="signin">로그인</a>
-        </div>
+<body class="h-full container-center">
+  <div>
+    <h1>회원가입</h1>
+
+    <form action="signup" method="post">
+      <div><input type="email" name="email" placeholder="email" autofocus required></div>
+      <div><input type="password" name="pw" placeholder="password" required></div>
+      <div><input type="text" name="nm" placeholder="name" require></div>
+      <div>
+        <input type="submit" value="회원가입">
+      </div>
+    </form>
+    <div>
+      <a href="signin">로그인</a>
     </div>
+  </div>
 </body>
+
 </html>
