621a80424e5cbd81ec4c4d4505826ea4ac9f448b
macadamia94 <thswlgus15@hanmail.net>
2022-07-02 11:20:00 +0000

    7/1

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -28,13 +28,17 @@ class UserModel extends Model {
     return $stmt->fetch(PDO::FETCH_OBJ);    // 값이 없을 경우 false가 넘어감
   }
 
-  public function selUserByIuser(&$param) {
-    $sql = "SELECT iuser, email, nm, cmt, mainimg, regdt 
-              FROM t_user
-             WHERE iuser = :iuser";
+  public function selUserProfile(&$param) {
+    $feediuser = $param["feediuser"];
+    $loginiuser = $param["loginiuser"];
+    $sql = "SELECT iuser, email, nm, cmt, mainimg
+                  , (SELECT COUNT(ifeed) FROM t_feed WHERE iuser = {$feediuser}) AS feedcnt
+                  ,	(SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser = {$feediuser} AND toiuser ={$loginiuser}) AS youme
+                  ,	(SELECT COUNT(toiuser) FROM t_user_follow WHERE fromiuser = {$loginiuser} AND toiuser = {$feediuser}) AS meyou
+            FROM t_user
+            WHERE iuser = {$feediuser}"; 
     $stmt = $this->pdo->prepare($sql);
-    $stmt->bindValue(":iuser", $param["iuser"]);
     $stmt->execute();
     return $stmt->fetch(PDO::FETCH_OBJ);
-}
+  }
 }
